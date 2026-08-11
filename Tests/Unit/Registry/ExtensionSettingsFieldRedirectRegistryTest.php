0339354d0f68ab2ddc9e53aa5d3e57d45535a8eb
Jürgen Pietschmann <juergen@t3planet.de>
2026-08-11 11:32:11 +0530

    chore(tests): update license information in ExtensionSettingsFieldRedirectRegistryTest.php

--- Tests/Unit/Registry/ExtensionSettingsFieldRedirectRegistryTest.php
+++ Tests/Unit/Registry/ExtensionSettingsFieldRedirectRegistryTest.php
@@ -8,6 +8,13 @@ declare(strict_types=1);
  * (c) T3Planet / NITSAN Technologies <support@t3planet.de>
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * and COMMERCIAL-LICENSE.md files that were distributed with this source code.
  */
 
 namespace NITSAN\NsT3AF\Tests\Unit\Registry;
